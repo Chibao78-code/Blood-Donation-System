27de61802469ea30d8d8bbe157da6fcc773007e1
Chibao78-code <ddinhchibao@gmail.com>
2025-09-09 07:35:41 +0700

    1

--- src/BloodDonation.Application/obj/Debug/net9.0/BloodDonation.Application.AssemblyInfo.cs
+++ src/BloodDonation.Application/obj/Debug/net9.0/BloodDonation.Application.AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloodDonation.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85a1ee788e9aa8feb0db53937f77bfed17cf87b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bc3baa87dc8afc77ef557f0469ac4db77c711ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloodDonation.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloodDonation.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
