c1c19d6c41cfce84d7dc95bb4e55f5933f0a02d5
Chibao78-code <ddinhchibao@gmail.com>
2025-09-12 21:14:38 +0700

    fix

--- src/BloodDonation.Domain/obj/Debug/net9.0/BloodDonation.Domain.AssemblyInfo.cs
+++ src/BloodDonation.Domain/obj/Debug/net9.0/BloodDonation.Domain.AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloodDonation.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27de61802469ea30d8d8bbe157da6fcc773007e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cd5e438dea3bb13ce40f84c767782702619e8c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloodDonation.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloodDonation.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
