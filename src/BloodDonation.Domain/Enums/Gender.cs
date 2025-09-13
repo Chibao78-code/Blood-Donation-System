8380bc3eeb65dc6799230acd07dda2b171377e45
Chibao78-code <ddinhchibao@gmail.com>
2025-09-13 13:45:35 +0700

    gender

---
+++ src/BloodDonation.Domain/Enums/Gender.cs
@@ -0,0 +1,22 @@
+namespace BloodDonation.Domain.Enums;
+
+/// <summary>
+/// Giới tính của người dùng trong hệ thống
+/// </summary>
+public enum Gender
+{
+    /// <summary>
+    /// Chưa xác định hoặc không muốn tiết lộ
+    /// </summary>
+    Other = 0,
+    
+    /// <summary>
+    /// Nam giới
+    /// </summary>
+    Male = 1,
+    
+    /// <summary>
+    /// Nữ giới  
+    /// </summary>
+    Female = 2
+}
--- src/BloodDonation.Infrastructure/obj/Debug/net9.0/BloodDonation.Infrastructure.AssemblyInfo.cs
+++ src/BloodDonation.Infrastructure/obj/Debug/net9.0/BloodDonation.Infrastructure.AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloodDonation.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+684151b75bbf283671d4f80691b95ee3c6ea7417")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8911f21e5c2d12497c4362744a6c8a7553d22db5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloodDonation.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloodDonation.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
