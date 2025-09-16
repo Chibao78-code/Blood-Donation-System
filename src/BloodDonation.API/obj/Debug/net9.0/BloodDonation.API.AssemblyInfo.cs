4d2fa684f727943c120f3459a57c57df23a340ef
Chibao78-code <ddinhchibao@gmail.com>
2025-09-16 14:21:19 +0700

    .

--- src/BloodDonation.API/obj/Debug/net9.0/BloodDonation.API.AssemblyInfo.cs
+++ src/BloodDonation.API/obj/Debug/net9.0/BloodDonation.API.AssemblyInfo.cs
@@ -6,9 +6,10 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloodDonation.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27de61802469ea30d8d8bbe157da6fcc773007e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54cee243f3794b37c670db21f6356bd305ece611")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloodDonation.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloodDonation.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+
 
 
