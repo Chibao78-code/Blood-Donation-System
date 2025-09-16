42233ba38fbf097b1b14177379ddbb236c671f06
Chibao78-code <ddinhchibao@gmail.com>
2025-09-16 07:30:37 +0700

    fix

--- src/BloodDonation.Application/obj/Debug/net9.0/BloodDonation.Application.AssemblyInfo.cs
+++ src/BloodDonation.Application/obj/Debug/net9.0/BloodDonation.Application.AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloodDonation.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8911f21e5c2d12497c4362744a6c8a7553d22db5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac7d20840011f3194896775abe959aff015f8803")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloodDonation.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloodDonation.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
